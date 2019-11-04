83e973b0cd7000f312126d5ebb9d931747bed735
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-11-04 14:08:33 +0100

    AngularFire setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBN45vfcuC3bqNUWLjCDjsPMQt11z7UZoQ',
+    authDomain: 'ionic4-fire-969f2.firebaseapp.com',
+    databaseURL: 'https://ionic4-fire-969f2.firebaseio.com',
+    projectId: 'ionic4-fire-969f2',
+    storageBucket: 'ionic4-fire-969f2.appspot.com',
+    messagingSenderId: '137265492326',
+    appId: '1:137265492326:web:6f475af002e40f90dd18a7'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBN45vfcuC3bqNUWLjCDjsPMQt11z7UZoQ',
+    authDomain: 'ionic4-fire-969f2.firebaseapp.com',
+    databaseURL: 'https://ionic4-fire-969f2.firebaseio.com',
+    projectId: 'ionic4-fire-969f2',
+    storageBucket: 'ionic4-fire-969f2.appspot.com',
+    messagingSenderId: '137265492326',
+    appId: '1:137265492326:web:6f475af002e40f90dd18a7'
+  }
 };
 
 /*
